b77142e7989773fac8f8edc9c4edd597a03e6248
sarang2551 <sarang.nirwan@gmail.com>
2024-12-27 13:17:17 +0800

    Gitignore addition

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './views/Home'
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="*" element={<h1>Not Found</h1>}/>
+    </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ frontend/views/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Home() {
+  return <h1>Add Expense</h1>;
+}
+
+export default Home;
